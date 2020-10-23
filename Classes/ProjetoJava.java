693158923fe85e47dbbe42035dfbc38a64816746
Fernando Figueiredo <nandofigueiredo.2010@gmail.com>
2020-10-23 19:20:41 -0300

    Feito a tela de login e inicio da tela principal

---
+++ Classes/ProjetoJava.java
@@ -0,0 +1,17 @@
+
+package Classes;
+
+import Formularios.frmLogin;
+
+
+public class ProjetoJava {
+
+    
+    public static void main(String[] args) {
+        frmLogin Login = new frmLogin();
+        Login.setLocationRelativeTo(null);
+        Login.setVisible(true);
+        
+    }
+    
+}
