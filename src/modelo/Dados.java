6ccbefe308721bfab788e513d9909b369e27f621
Fernando Figueiredo <nandofigueiredo.2010@gmail.com>
2020-10-28 13:58:02 -0300

    Projeto Java

---
+++ src/modelo/Dados.java
@@ -0,0 +1,13 @@
+
+package modelo;
+
+public class Dados {
+    public boolean validarUsuarios(String usuario, String senha){
+        if(usuario.equals("fernando") && senha.equals("123")){
+            return true;
+         
+        }else{
+            return false;
+        }
+    }
+}
---
+++ src/modelo/ProjetoJava.java
@@ -0,0 +1,17 @@
+
+package modelo;
+
+import visao.Login;
+
+
+public class ProjetoJava {
+
+    
+    public static void main(String[] args) {
+        Login Login = new Login();
+        Login.setLocationRelativeTo(null);
+        Login.setVisible(true);
+        
+    }
+    
+}
